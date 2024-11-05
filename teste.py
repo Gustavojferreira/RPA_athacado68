bcdec0461fefeb0483d765649940679021eac27a
Gustavo F. <132690374+Gustavojferreira@users.noreply.github.com>
2024-11-05 12:11:48 -0300

    Delete teste.py

--- teste.py
+++
@@ -1,8 +0,0 @@
-import time
-import pyautogui
-
-time.sleep(2)
-encontrar_img = pyautogui.locateCenterOnScreen('teste.png')  #Print do campo de situação "A recepcionar"
-
-if encontrar_img:
-    print("funcionou")
